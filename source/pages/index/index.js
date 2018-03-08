fdfa66c040aecaa8cfccf49cf3d21f3db4cf36ba
Tony Chupak <tony.chupak@gmail.com>
2018-03-08 16:14:54 +0200

    section Roadmap

--- source/pages/index/index.js
+++ source/pages/index/index.js
@@ -4,3 +4,18 @@ import 'ress';
 // import images
 import './images/logo.svg';
 import './images/bg_tooltip.svg';
+
+const nav = document.getElementById('navigation');
+const navClass = 'navigation';
+
+const onScroll = () => {
+  const topOffset = window.pageYOffset;
+  if (topOffset >= 830) {
+    nav.className = `${navClass} is-fixed`;
+  } else {
+    nav.className = navClass;
+  };
+  console.log(topOffset);
+}
+window.addEventListener('scroll', onScroll);
+// console.log(nav);
